b403bd0c2cc2029b3a9c1be1f3e370b9569bd49b
MarioClot <mariorrsv@gmail.com>
2018-05-16 22:04:10 +0200

    Genera PDF browser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -43,14 +43,54 @@ export class HomePage extends Utils{
   generatePDF(){
     // TODO: this.arrayJugadors estan els jugadors guardats, on puc
     //      accedir a dorsal, punts...
-    /*var docDefinition = {
-      content: [
+    var body = [['Dorsal', 'Punts', 'Faltes', 'Rebots', 'Asistencies']];
 
-      ],
+    this.arrayJugadors.forEach((element) => {
+      body.push([{text: element.key, style: 'tableKey'}, element.punts, element.faltes, element.rebots, element.asistencies])
+    })
+
+    var docDefinition = {
+      content: {
+          style: 'tablezebra',
+          table: {
+            body
+          },
+          layout: {
+            fillColor: function (i, node) {
+              if( i == 0 )
+                return '#8E8E8E';
+              else
+                return (i % 2 === 0) ? '#CCCCCC' : null;
+            }
+          }
+      },
       styles: {
-
+        tablezebra: {
+          margin: [0, 5, 0, 15]
+        },
+        tableKey: {
+          bold: true,
+          color: 'black',
+        }
       }
     }
-    this.pdfObj = pdfMake.createPdf(docDefinition);*/ 
+    this.pdfObj = pdfMake.createPdf(docDefinition);
+  }
+
+  downloadPdf() {
+    this.generatePDF();
+    if (this.plt.is('cordova')) {
+      this.pdfObj.getBuffer((buffer) => {
+        var blob = new Blob([buffer], { type: 'application/pdf' });
+ 
+        // Save the PDF to the data Directory of our App
+        this.file.writeFile(this.file.dataDirectory, 'stats.pdf', blob, { replace: true }).then(fileEntry => {
+          // Open the PDf with the correct OS tools
+          this.fileOpener.open(this.file.dataDirectory + 'stats.pdf', 'application/pdf');
+        })
+      });
+    } else {
+      this.pdfObj.download();
+    }
   }
 }
